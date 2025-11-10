69c78e941d7cdb69162074d4d47c4284486d8032
shin-nowa <kkeusoeu@gmail.com>
2025-11-09 23:25:16 -0300

    Foto de perfil voltou a funcionar

--- localFileTransferer/localFileTransferer/urls.py
+++ localFileTransferer/localFileTransferer/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +26,5 @@ urlpatterns = [
     
     path('', include('gerenciador.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
